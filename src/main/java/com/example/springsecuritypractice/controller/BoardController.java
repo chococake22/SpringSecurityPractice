e24f229bc7444c8799adc28cf7b7c23493987bb7
chococake22 <fightest21@gmail.com>
2022-01-11 17:45:46 +0900

    2022. 01. 11

--- src/main/java/com/example/springsecuritypractice/controller/BoardController.java
+++ src/main/java/com/example/springsecuritypractice/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.example.springsecuritypractice.controller;
 
+import com.example.springsecuritypractice.account.UserAccount;
 import com.example.springsecuritypractice.board.Board;
 import com.example.springsecuritypractice.board.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -22,10 +27,17 @@ public class BoardController {
         return "/board/boardList";
     }
 
-    @GetMapping("/boardWriteForm")
+    @GetMapping("/boardWrite")
     public String boardForm(Model model) {
-
+        model.addAttribute("board", new Board());
         return "/board/boardWriteForm";
+    }
+
+    @PostMapping("/boardWrite")
+    public String boardWriteCheck(@ModelAttribute Board board, @AuthenticationPrincipal UserAccount userAccount) {
+        board.setAuthor(userAccount.getAccount().getUsername());
+        boardRepository.save(board);
+        return "redirect:/list";
     }
 
 }
